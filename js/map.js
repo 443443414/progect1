dabeca11669d2763a706efbc73045c10c8557959
443443414 <57660688+443443414@users.noreply.github.com>
2019-11-12 22:22:19 +0300

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,12 +1,7 @@
 var map = L.map('map').setView([60.00, 30.00], 9);
 var layer1 = L.tileLayer("https://api.mapbox.com/styles/v1/443443414/ck2rz7yp21i1t1cp39rz58r7p/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiNDQzNDQzNDE0IiwiYSI6ImNrMjRmbGloaDB0anMzY21xcWM3dnVwYWQifQ.rQXgw2v8C1LR8MB2oUl0Ig").addTo(map);
 
-var Myicon = L.icon({
-  iconUrl: '../assets/images/point_icon.png',
-  iconSize: [27, 31],
-  iconAnchor: [13.5, 17.5],
-  popupAnchor: [0, -11]
-});
+
 
 
 function setBasemap(basemap) {
@@ -44,10 +39,25 @@ function setBasemap(basemap) {
 
 
 /*условие кнопки*/
-var my_layer = L.geoJSON (wind_data,{icon: Myicon});
-my_layer.bindPopup(function () {
-  return L.Util.template("Я буду очень благодарен, если Дмитрий Сергеевич поможет мне научиться добавлять всплывающие окна с атрибутами слоя");
-});
+var my_layer = L.geoJSON(wind_data, {
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup('<strong>Скорость ветра на высоте 10 м:</strong><br>' +
+      'Январь ' + feature.properties.field_4 + '<br>' +
+      'Февраль ' + feature.properties.field_5 + '<br>' +
+      'Март ' + feature.properties.field_6 + '<br>' +
+      'Апрель ' + feature.properties.field_7 + '<br>' +
+      'Май ' + feature.properties.field_8 + '<br>' +
+      'Июнь ' + feature.properties.field_9 + '<br>' +
+      'Июль ' + feature.properties.field_10 + '<br>' +
+      'Август ' + feature.properties.field_11 + '<br>' +
+      'Сентябрь ' + feature.properties.field_12 + '<br>' +
+      'Октябрь ' + feature.properties.field_13 + '<br>' +
+      'Ноябрь ' + feature.properties.field_14 + '<br>' +
+      'Декабрь ' + feature.properties.field_15 + '<br>'
+    );
+  }
+}).addTo(map);
+
 var my_layershown = false;
 
 
