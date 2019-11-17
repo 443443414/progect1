5b47319af0bcd6631752d716615bcb30d5c87e15
443443414 <57660688+443443414@users.noreply.github.com>
2019-11-17 19:08:39 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,16 +1,12 @@
 var map = L.map('map').setView([60.00, 31.60], 7);
 var layer1 = L.tileLayer("https://api.mapbox.com/styles/v1/443443414/ck2rz7yp21i1t1cp39rz58r7p/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiNDQzNDQzNDE0IiwiYSI6ImNrMjRmbGloaDB0anMzY21xcWM3dnVwYWQifQ.rQXgw2v8C1LR8MB2oUl0Ig").addTo(map);
-
 var Fuckingicon = L.icon({
   iconUrl: '../assets/images/point_icon.png',
-
   iconSize: [20, 20], // size of the icon
   shadowSize: [50, 64], // size of the shadow
   iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
   popupAnchor: [0, 00] // point from which the popup should open relative to the iconAnchor
 });
-
-
 function setBasemap(basemap) {
   if (layer1) {
     map.removeLayer(layer1);
@@ -21,13 +17,10 @@ function setBasemap(basemap) {
   } else {
     layer1 = L.esri.basemapLayer(basemap);
   }
-
   map.addLayer(layer1);
-
   if (layerLabels) {
     map.removeLayer(layerLabels);
   }
-
   if (
     basemap === 'ShadedRelief' ||
     basemap === 'Oceans' ||
@@ -42,10 +35,6 @@ function setBasemap(basemap) {
     map.addLayer(layerLabels);
   }
 }
-
-
-
-
 /*условие кнопки*/
 var my_layer = L.geoJSON(wind_data, {
   pointToLayer: function (feature, latlng) {
@@ -70,12 +59,7 @@ var my_layer = L.geoJSON(wind_data, {
     );
   }
 });
-
 var my_layershown = false;
-
-
-
-
 function displayLayer() {
   if (my_layershown) {
     document.getElementById("layerbtn-title").innerHTML = "Show";
@@ -86,17 +70,12 @@ function displayLayer() {
   }
   my_layershown = !my_layershown;
 };
-
-
-
-
 document
   .querySelector('#basemaps')
   .addEventListener('change', function (e) {
     var basemap = e.target.value;
     setBasemap(basemap);
   });
-
 /*запуск кнопки*/
 document
   .addEventListener("DOMContentLoaded", function () {
@@ -104,22 +83,3 @@ document
       displayLayer();
     });
   });
-
-
-/*my_json = L.geoJson(data, {
-          pointToLayer: function(feature, latlng) {
-              var smallIcon = L.Icon({
-                  options: {
-                      iconSize: [27, 27],
-                      iconAnchor: [13, 27],
-                      popupAnchor:  [1, -24],
-                      iconUrl: 'icone/chapel-2.png'
-                  }
-              });
-              return L.marker(latlng, {icon: smallIcon});
-          },
-         onEachFeature: function (feature, layer) {
-                 layer.bindPopup(feature.properties.ATT1 + '<br />'
-                                               + feature.properties.ATT2);
-         }
-       });*/
